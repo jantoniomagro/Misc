e17210461590b31ecf6e18d96a666712b2cc01c7
Jose <jantoniomagro@users.noreply.github.com>
2021-04-26 22:40:37 +0200

    Update QueryExampleClient.java
    primer commit en rama pruebas

--- QueryExampleClient.java
+++ QueryExampleClient.java
@@ -1,7 +1,7 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * test en rama "pruebas"
  */
+
 package alma.archive.jportal.opencadc;
 
 import ca.nrc.cadc.uws.Job;
